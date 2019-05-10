6991962c1143f38666bb7d45756c1d602262d2bd
甜乐 宋 <2314800304@qq.com>
2019-05-11 00:20:17 +0800

    添加注册、同步通讯录页面

--- app/src/main/java/com/androidgroup5/onlinecontact/MainActivity.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/MainActivity.java
@@ -1,13 +1,120 @@
 package com.androidgroup5.onlinecontact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn01,btn02,
+            btn11,btn12,
+            btn21,btn22,
+            btn31,btn32,
+            btn41,btn42,
+            btn51,btn52,
+            btn61,btn62,
+            btn71,btn72,
+            btn81,btn82,
+            btn91,btn92;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    public void init(){
+        btn01=(Button)findViewById(R.id.btn_01);
+        btn01.setOnClickListener(new mClick(1));
+        btn02=(Button)findViewById(R.id.btn_02);
+        btn02.setOnClickListener(new mClick(2));
+        btn11=(Button)findViewById(R.id.btn_11);
+        btn11.setOnClickListener(new mClick(11));
+        btn12=(Button)findViewById(R.id.btn_12);
+        btn12.setOnClickListener(new mClick(12));
+        btn21=(Button)findViewById(R.id.btn_21);
+        btn21.setOnClickListener(new mClick(21));
+        btn22=(Button)findViewById(R.id.btn_22);
+        btn22.setOnClickListener(new mClick(22));
+        btn31=(Button)findViewById(R.id.btn_31);
+        btn31.setOnClickListener(new mClick(31));
+        btn32=(Button)findViewById(R.id.btn_32);
+        btn32.setOnClickListener(new mClick(32));
+        btn41=(Button)findViewById(R.id.btn_41);
+        btn41.setOnClickListener(new mClick(41));
+        btn42=(Button)findViewById(R.id.btn_42);
+        btn42.setOnClickListener(new mClick(42));
+        btn51=(Button)findViewById(R.id.btn_51);
+        btn51.setOnClickListener(new mClick(51));
+        btn52=(Button)findViewById(R.id.btn_52);
+        btn52.setOnClickListener(new mClick(52));
+        btn61=(Button)findViewById(R.id.btn_61);
+        btn61.setOnClickListener(new mClick(61));
+        btn62=(Button)findViewById(R.id.btn_62);
+        btn62.setOnClickListener(new mClick(62));
+        btn71=(Button)findViewById(R.id.btn_71);
+        btn71.setOnClickListener(new mClick(71));
+        btn72=(Button)findViewById(R.id.btn_72);
+        btn72.setOnClickListener(new mClick(72));
+        btn81=(Button)findViewById(R.id.btn_81);
+        btn81.setOnClickListener(new mClick(81));
+        btn82=(Button)findViewById(R.id.btn_82);
+        btn82.setOnClickListener(new mClick(82));
+        btn91=(Button)findViewById(R.id.btn_91);
+        btn91.setOnClickListener(new mClick(91));
+        btn92=(Button)findViewById(R.id.btn_92);
+        btn92.setOnClickListener(new mClick(92));
+    }
+    class mClick implements View.OnClickListener{
+        int _id;
+        public mClick(int id){
+            _id=id;
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (_id){
+                case 1:
+                    startActivity(new Intent().setClass(MainActivity.this,Register.class));
+                    break;
+                case 2:
+                    startActivity(new Intent().setClass(MainActivity.this,SyncAddressBook.class).putExtra("user_id","100001"));
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 21:
+                    break;
+                case 22:
+                    break;
+                case 31:
+                    break;
+                case 32:
+                    break;
+                case 41:
+                    break;
+                case 42:
+                    break;
+                case 51:
+                    break;
+                case 52:
+                    break;
+                case 61:
+                    break;
+                case 71:
+                    break;
+                case 72:
+                    break;
+                case 81:
+                    break;
+                case 82:
+                    break;
+                case 91:
+                    break;
+                case 92:
+                    break;
+            }
+        }
     }
 }
