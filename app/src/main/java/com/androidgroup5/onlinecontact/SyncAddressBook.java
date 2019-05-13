26f5f982c4dc63110055a0dc386fb837483977e5
甜乐 宋 <2314800304@qq.com>
2019-05-13 10:30:34 +0800

    添加注册、通讯录同步处理逻辑

--- app/src/main/java/com/androidgroup5/onlinecontact/SyncAddressBook.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/SyncAddressBook.java
@@ -2,6 +2,10 @@ package com.androidgroup5.onlinecontact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class SyncAddressBook extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class SyncAddressBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_address_book);
+        ((Button)findViewById(R.id.btn_sync)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox)findViewById(R.id.ckb_local)).isChecked()){
+                    if(UpdateLocal()){
+                        Toast.makeText(SyncAddressBook.this,"本地通讯录更新成功！",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(SyncAddressBook.this,"本地通讯录更新失败，请重试！",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(((CheckBox)findViewById(R.id.ckb_cloud)).isChecked()){
+                    if(UpdateCloud()){
+                        Toast.makeText(SyncAddressBook.this,"云端通讯录更新成功！",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(SyncAddressBook.this,"云端通讯录更新失败，请重试！",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+    private boolean UpdateLocal(){
+        return true;
+    }
+    private boolean UpdateCloud(){
+        return true;
     }
 }
