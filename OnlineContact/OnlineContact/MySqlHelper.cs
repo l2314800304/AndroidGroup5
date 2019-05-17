c8bc33966fb1e5cd0af18ca1fdbdbd1bc487c723
甜乐 宋 <2314800304@qq.com>
2019-05-17 11:20:58 +0800

    ...

---
+++ OnlineContact/OnlineContact/MySqlHelper.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace OnlineContact
+{
+    public class MySqlHelper
+    {
+        public static string Constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+        #region  建立MySql数据库连接
+        public MySqlConnection getMySqlCon()
+        {
+            string M_str_sqlcon = Constr;
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            return myCon;
+        }
+        #endregion
+
+        #region  执行MySqlCommand命令
+        /// <summary>
+        /// 执行MySqlCommand,返回影响的行数
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public int getMySqlCom(string M_str_sqlstr, params MySqlParameter[] parameters)
+        {
+            MySqlConnection mysqlcon = this.getMySqlCon();
+            mysqlcon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcom.Parameters.AddRange(parameters);
+            int count = mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            mysqlcon.Close();
+            mysqlcon.Dispose();
+            return count;
+        }
+        #endregion
+
+        #region  创建MySqlDataReader对象
+        /// <summary>
+        /// 执行查询语句,返回MySqlDataReader
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getMySqlReader(string M_str_sqlstr, params MySqlParameter[] parameters)
+        {
+            MySqlConnection mysqlcon = this.getMySqlCon();
+            mysqlcon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcom.Parameters.AddRange(parameters);
+            MySqlDataReader reader = mysqlcom.ExecuteReader();
+            return reader;
+        }
+        #endregion
+    }
+}
---
+++ OnlineContact/OnlineContact/Register.ashx.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineContact
+{
+    /// <summary>
+    /// 账号注册
+    /// </summary>
+    public class Register : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            //获取请求参数
+            String UserName = context.Request["UserName"],
+                Password = context.Request["Password"],
+                Sex = context.Request["Sex"],
+                Location = context.Request["Location"],
+                Remark = context.Request["Remark"];
+            //构建数据库操作语句
+            String sql = "insert into user (UserName,Password,Sex,Location,Remark) Values (@u,@p,@s,@l,@r);";
+            //构建参数数组
+            MySqlParameter[] pms={
+                new MySqlParameter("@u",UserName),
+                new MySqlParameter("@p", Password),
+                new MySqlParameter("@s", Sex),
+                new MySqlParameter("@l", Location),
+                new MySqlParameter("@r", Remark)
+            };
+            //创建帮助类（自己写的）
+            MySqlHelper helper = new MySqlHelper();
+            //调用帮助类中的方法
+            //getMySqlReader()对数据库进行查询，返回MySqlDataReader
+            //getMySqlCom()对数据库进行增删改操作，返回影响行数
+            //进行相关判断
+            if (helper.getMySqlCom(sql, pms)>0)
+            {
+                //返回响应信息
+                context.Response.Write("OK");
+            }
+            else
+            {
+                context.Response.Write("Error");
+            }
+            //进行测试
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
