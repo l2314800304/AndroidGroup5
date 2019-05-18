f38385d012d93c250c87880c7e0e16a54cc0bb2a
ssx0115 <50482877+ssx0115@users.noreply.github.com>
2019-05-18 19:32:04 +0800

    查找

---
+++ app/src/main/java/com/androidgroup5/onlinecontact/Find.java
@@ -0,0 +1,15 @@
+package com.androidgroup5.onlinecontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Find extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_find);
+    }
+
+
+}
