342401f5241e75056c5ed7c8039fee447a99bbf2
clumsygoose <3237327416@qq.com>
2019-05-20 18:03:53 +0800

    shukai

---
+++ app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
@@ -0,0 +1,21 @@
+package com.androidgroup5.onlinecontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
+
+public class Insert extends AppCompatActivity {
+    //通用的显示对话框的方法
+    private void showDialog(String title, String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //设置对话框的标题
+        builder.setTitle(title);
+        //设置对话框显示的信息
+        builder.setMessage(msg);
+        //设置对话框的按钮
+        builder.setPositiveButton("确定", null);
+        //显示对话框
+        builder.create().show();
+        Intent intent;
+    }
+}
