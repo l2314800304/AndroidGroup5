c654556091e672c404550e02bdad883a78f5ccad
1012600207@qq.com <1012600207@qq.com>
2019-05-24 17:16:16 +0800

    1

--- OnlineContact/OnlineContact/UpdateContactInfo.ashx.cs
+++ OnlineContact/OnlineContact/UpdateContactInfo.ashx.cs
@@ -17,18 +17,18 @@ namespace OnlineContact
         {
             context.Response.ContentType = "text/plain";
             //获取请求参数
-            int contact_info_ID = Convert.ToInt16(context.Request["contact_info_ID"]);
+            int contact_info_ID = Convert.ToInt16(context.Request["contact_info_ID"]),
+            contact_email= Convert.ToInt16(context.Request["contact_email"]);
             string contact_number = context.Request["contact_number"].ToString(),
-              contact_email = context.Request["contact_email"].ToString(),
               contact_type = context.Request["contact_type"].ToString();
 
             StringBuilder stb = new StringBuilder();
             stb.Append("update contact_info set Number= '");
             stb.Append(contact_number);
             stb.Append("',");
-            stb.Append("EmailorNumber='");
+            stb.Append("EmailorNumber=");
             stb.Append(contact_email);
-            stb.Append("',");
+            stb.Append(",");
             stb.Append("Type='");
             stb.Append(contact_type);
             stb.Append("' where ID=");
