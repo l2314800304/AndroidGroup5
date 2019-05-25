85b25d1ed8d9ae525767871fbaa69acf26b6e265
clumsygoose <3237327416@qq.com>
2019-05-25 12:26:00 +0800

    Merge branch 'master' of E:\AndroidProject\AndroidGroup5 with conflicts.

--- app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
@@ -6,6 +6,21 @@ import android.app.AlertDialog;
 import android.content.Intent;
 
 public class Insert extends AppCompatActivity {
+    //通用的显示对话框的方法
+    private void showDialog(String title, String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //设置对话框显示的标题
+        builder.setTitle(title);
+        //设置对话框现实的信息
+        builder.setMessage(msg);
+        //设置对话框的按钮
+        builder.setPositiveButton("确定", null);
+        //显示对话框
+        builder.create().show();
+        Intent intent;
+    }
+    //两个按钮共用一个单击事件方法，通过按钮的id区分单击了哪个按钮
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
