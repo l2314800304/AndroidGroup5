b2a0729923be416121cf584026b8be52d60f3662
clumsygoose <3237327416@qq.com>
2019-05-26 23:41:44 +0800

    袁陈写的

---
+++ app/src/main/java/com/androidgroup5/onlinecontact/XinxiActivity.java
@@ -0,0 +1,16 @@
+package com.androidgroup5.onlinecontact;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2019/5/26.
+ */
+
+public class XinxiActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xinxi);
+    }
+}
