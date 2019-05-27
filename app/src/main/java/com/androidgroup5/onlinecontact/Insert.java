73a94df21132154980418d8a2e1586361a5a1594
clumsygoose <3237327416@qq.com>
2019-05-27 23:47:57 +0800

    ...

--- app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
@@ -2,8 +2,21 @@ package com.androidgroup5.onlinecontact;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Insert extends AppCompatActivity {
+    private TextView contactName;
+    private TextView contactEmail;
+    private TextView contactMobilePhone;
+    private TextView contactTelPhone;
+    private TextView contactCorpPhone;
+    private TextView contactPosition;
+    private TextView contactOtherInfo;
+
+    private void init() {
+        //contactName = (TextView)findViewById()
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
