81d65f1972f6f6c5897bc896c7dace02596113c0
wuwensen007 <2086846171@qq.com>
2019-05-27 11:09:10 +0800

    更新了login和showContactActivity

--- app/src/main/java/com/androidgroup5/onlinecontact/ShowContactActivity.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/ShowContactActivity.java
@@ -3,30 +3,43 @@ package com.androidgroup5.onlinecontact;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.androidgroup5.onlinecontact.EntityClass.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ShowContactActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listView;
 
-    private ArrayList<Contact> contactList = new ArrayList<>();
+    private ArrayList<HashMap<String, Object>> contactList = new ArrayList<>();
 
-    private List<String> names = new ArrayList<>();
+    private ArrayList<String> names = new ArrayList<>();
 
-    private User user;
+    private int userId;
+
+    private String userName;
 
     private BaseAdapter adapter;
 
@@ -35,7 +48,8 @@ public class ShowContactActivity extends AppCompatActivity implements AdapterVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_contact);
         Intent intent = getIntent();
-        user = (User) intent.getSerializableExtra("user");
+        userName =  intent.getStringExtra("UserName");
+        userId = Integer.parseInt(intent.getStringExtra("ID"));
         init();
     }
 
@@ -43,10 +57,11 @@ public class ShowContactActivity extends AppCompatActivity implements AdapterVie
     public void init(){
 
         listView = findViewById(R.id.listView);
-        fetchData(16);
+        fetchData(userId);
         adapter = new ArrayAdapter<String>(ShowContactActivity.this, android.R.layout.simple_list_item_1, names);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        listView.setOnItemClickListener(ShowContactActivity.this);
+
     }
 
 
@@ -54,24 +69,21 @@ public class ShowContactActivity extends AppCompatActivity implements AdapterVie
 
     private void fetchData(int id){
 
-        for (int i = 0; i < 100; i++){
-//            Contact contact = new Contact();
-//            contact.setID(i);
-//            contact.setName("张三"+i);
-//            contactList.add(new Contact());
-            names.add("张三"+i);
-        }
+        names.add(userName);
 
-        Toast.makeText(ShowContactActivity.this,id+"-------",Toast.LENGTH_LONG).show();
-//        URLEncoder.encode(user)
-        // 根据id获取contactSet
-//        OkHttpClient client = new OkHttpClient();
-//        Request request = new Request.Builder().url("http://192.168.43.199:8080/api/users/"+URLEncoder.encode(String.valueOf(id))+"/contactSet").method("GET",null).build();
+//        OkHttpClient client = new OkHttpClient.Builder()
+//                .connectTimeout(10, TimeUnit.SECONDS)
+//                .readTimeout(20, TimeUnit.SECONDS)
+//                .writeTimeout(20, TimeUnit.SECONDS)
+//                .build();
+//        Request request = new Request.Builder().url("http://wuwensen007.ticp.io/contacts?userId=" + id).get().build();
+//
+//
 //
 //        client.newCall(request).enqueue(new Callback() {
 //            @Override
 //            public void onFailure(Call call, IOException e) {
-//
+//                Toast.makeText(ShowContactActivity.this,"连接失败",Toast.LENGTH_LONG).show();
 //            }
 //
 //            @Override
@@ -79,14 +91,12 @@ public class ShowContactActivity extends AppCompatActivity implements AdapterVie
 //                if (response.isSuccessful()){
 //                    // 加载个人联系人列表
 //
-//                    Toast.makeText(ShowContactActivity.this,response.code()+"-------",Toast.LENGTH_LONG).show();
+//                    Gson gson = new Gson();
+//                    String json =  response.body().string();
 //
-////                    Gson gson = new Gson();
-////                    contactList = gson.fromJson(response.body().string(),  new TypeToken<ArrayList<Contact>>(){}.getType());
-////
-////                    for (Contact i: contactList){
-////                        names.add(i.getName());
-////                    }
+////                    Toast.makeText(ShowContactActivity.this,json,Toast.LENGTH_LONG).show();
+//                    List<String> data = gson.fromJson(json, new TypeToken< ArrayList<String>>(){}.getType());
+//                    names.addAll(data);
 //
 //                }else {
 //                    // 用户名或密码错误
