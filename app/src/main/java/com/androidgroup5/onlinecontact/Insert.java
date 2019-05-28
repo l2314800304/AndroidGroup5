3d85542d84c24cf71e14f2a7566fe35fed206f3b
clumsygoose <3237327416@qq.com>
2019-05-28 18:08:09 +0800

    ...

--- app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/Insert.java
@@ -2,6 +2,7 @@ package com.androidgroup5.onlinecontact;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Insert extends AppCompatActivity {
@@ -12,14 +13,27 @@ public class Insert extends AppCompatActivity {
     private TextView contactCorpPhone;
     private TextView contactPosition;
     private TextView contactOtherInfo;
+    private Button insert;
+    private Button clear;
 
     private void init() {
-        //contactName = (TextView)findViewById()
+        contactName = (TextView)findViewById(R.id.contactName);
+        contactEmail = (TextView)findViewById(R.id.contactEmail);
+        contactMobilePhone = (TextView)findViewById(R.id.contactMobilePhone);
+        contactTelPhone = (TextView)findViewById(R.id.contactCorpPhone);
+        contactPosition = (TextView)findViewById(R.id.Position);
+        contactOtherInfo = (TextView)findViewById(R.id.otherInfo);
+        insert = (Button)findViewById(R.id.btn_insert);
+        clear = (Button)findViewById(R.id.btn_clear);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        init();
+        //clear.setOnClickListener((v) -> {
+
+        //});
     }
 }
