73d54dd3ed33d8c9dd71d9e48ae67117e6c0e477
ssx0115 <50482877+ssx0115@users.noreply.github.com>
2019-05-28 19:19:19 +0800

    Add files via upload

--- app/src/main/java/com/androidgroup5/onlinecontact/Export.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/Export.java
@@ -3,13 +3,25 @@ package com.androidgroup5.onlinecontact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Export extends AppCompatActivity {
-
+    private Button btnExported;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
+        btnExported=(Button)findViewById(R.id.btnExported) ;
+        btnExported.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Export.this, "导出成功", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
