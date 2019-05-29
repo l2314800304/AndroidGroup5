49fb30f09b64a09eb5a69f4d316790d3b8cb8944
ssx0115 <50482877+ssx0115@users.noreply.github.com>
2019-05-29 20:20:56 +0800

    Add files via upload

--- app/src/main/java/com/androidgroup5/onlinecontact/search/sContact.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/search/sContact.java
@@ -6,12 +6,12 @@ import com.androidgroup5.onlinecontact.cn.CN;
 public class sContact implements CN {
 
     public final String name;
-
+    public final String Number;
     public final int imgUrl;
-
-    public sContact(String name, int imgUrl) {
+    public sContact(String name,String Number, int imgUrl) {
         this.name = name;
         this.imgUrl = imgUrl;
+        this.Number = Number;
     }
 
     @Override
