1ea075e9413918680c02c1cbcd505bf6616197b2
clumsygoose <3237327416@qq.com>
2019-06-04 23:33:08 +0800

    yuanchen1232

--- app/src/main/java/com/androidgroup5/onlinecontact/XinxiActivity.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/XinxiActivity.java
@@ -1,16 +1,80 @@
 package com.androidgroup5.onlinecontact;
-
-import android.app.Activity;
+import java.util.Calendar;
+        import android.app.Activity;
+        import android.app.DatePickerDialog;
+        import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.DatePicker;
+        import android.widget.TextView;
 
 /**
- * Created by Administrator on 2019/5/26.
+ * 时间选择器
  */
-
 public class XinxiActivity extends Activity {
+    int mYear, mMonth, mDay;
+    Button btn;
+    TextView dateDisplay;
+    final int DATE_DIALOG = 1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xinxi_layout);
+
+        btn = (Button) findViewById(R.id.dateChoose);
+        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG);
+            }
+        });
+        Button btn8=findViewById(R.id.btntuichu);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it8=new Intent();
+                it8.setClass(XinxiActivity.this,SkipActivity.class);
+                XinxiActivity.this.startActivity(it8);
+
+            }
+        });
+        final Calendar ca = Calendar.getInstance();
+        mYear = ca.get(Calendar.YEAR);
+        mMonth = ca.get(Calendar.MONTH);
+        mDay = ca.get(Calendar.DAY_OF_MONTH);
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG:
+                return new DatePickerDialog(this, mdateListener, mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+    /**
+     * 设置日期 利用StringBuffer追加
+     */
+    public void display() {
+        dateDisplay.setText(new StringBuffer().append(mMonth + 1).append("-").append(mDay).append("-").append(mYear).append(" "));
+    }
+
+    private DatePickerDialog.OnDateSetListener mdateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            display();
+        }
+    };
 }
