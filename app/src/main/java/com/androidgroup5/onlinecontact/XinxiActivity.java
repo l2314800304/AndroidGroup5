14eb501fa3101ce96a70d9d549762bcff047725d
clumsygoose <3237327416@qq.com>
2019-06-10 23:02:31 +0800

    yuanchen1

--- app/src/main/java/com/androidgroup5/onlinecontact/XinxiActivity.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/XinxiActivity.java
@@ -1,26 +1,59 @@
 package com.androidgroup5.onlinecontact;
 import java.util.Calendar;
-        import android.app.Activity;
-        import android.app.DatePickerDialog;
-        import android.app.Dialog;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.widget.Button;
-        import android.widget.DatePicker;
-        import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * 时间选择器
  */
-public class XinxiActivity extends Activity {
+public class XinxiActivity extends AppCompatActivity {
     Button btn;
+    private EditText Password;
+    private EditText Username;
+    private EditText Sex;
+    private EditText Location;
+    private EditText Remark;
 
+    @SuppressLint("WrongViewCast")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xinxi_layout);
+        /*init();
+        String Remark=((UserParameter)getApplication()).getUser().getRemark();
+        buttonRemark.setText(Remark);
+        String Username=((UserParameter)getApplication()).getUser().getUserName();
+        buttonUsername.setText(Remark);
+        String Sex=((UserParameter)getApplication()).getUser().getSex();
+        buttonSex.setText(Remark);
+        String Location=((UserParameter)getApplication()).getUser().getLocation();
+        buttonLocation.setText(Remark);
+        String Password=((UserParameter)getApplication()).getUser().getPassword();
+        buttonPassword.setText(Remark);*/
+
+        Button btn9=findViewById(R.id.btntuichu);
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it9=new Intent();
+                it9.setClass(XinxiActivity.this,SkipActivity.class);
+                XinxiActivity.this.startActivity(it9);
+
+            }
+        });
         Button btn8=findViewById(R.id.btntuichu);
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +63,16 @@ public class XinxiActivity extends Activity {
                 XinxiActivity.this.startActivity(it8);
             }
         });
+
+       /* private void init() {
+            Username = (EditText) findViewById(R.id.buttonUsername);
+            Sex = (EditText) findViewById(R.id.buttonSex);
+            Location = (EditText) findViewById(R.id.buttonLocation);
+            Password = (EditText) findViewById(R.id.buttonPassword);
+            Remark= (EditText) findViewById(R.id.buttonRemark);
+
+        }*/
+
 
     }
 
