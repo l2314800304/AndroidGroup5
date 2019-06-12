d5d214d4f3f94d369eeeb3a6f0b655f8e8b6e44f
甜乐 宋 <2314800304@qq.com>
2019-06-12 09:12:10 +0800

    最终版本

--- app/src/main/java/com/androidgroup5/onlinecontact/SyncAddressBook.java
+++ app/src/main/java/com/androidgroup5/onlinecontact/SyncAddressBook.java
@@ -102,6 +102,17 @@ public class SyncAddressBook extends Activity {
                     m = "提取成功，正在同步通讯录信息以及通话信息，由于数据较多，请耐心等待...";
                     break;
                 case 14:
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            ;
+                            UserParameter p = (UserParameter) getApplication();
+                            User u = new User();
+                            u.setContact(GetContactFromLocal());
+                            u.setRecord(GetRecordFromLocal());
+                            p.setLocal(u);
+                        }
+                    }.start();
                     m = "恭喜，同步成功！";
                     break;
                 case -1:
@@ -114,6 +125,112 @@ public class SyncAddressBook extends Activity {
         }
 
     };
+
+    private List<Contact> GetContactFromLocal() {
+        List<Contact> contacts = new ArrayList<Contact>();
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        ContentResolver contentResolver = this.getContentResolver();
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Contact contact = new Contact();
+            List<ContactInfos> contactInfos = new ArrayList<ContactInfos>();
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            contact.setName(name);
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phones = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
+                    null, null);
+            contact.setID(new Integer(id));
+            while (phones.moveToNext()) {
+                ContactInfos info = new ContactInfos();
+                String phoneNumber = phones.getString(phones.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                //添加Phone的信息
+                String phoneType = phones.getString(phones.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.TYPE));
+                info.setEmailOrNumber(5);
+                info.setNumber(phoneNumber);
+                info.setType(phoneType);
+                contactInfos.add(info);
+            }
+            phones.close();
+            Cursor emails = contentResolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + id,
+                    null, null);
+            while (emails.moveToNext()) {
+                ContactInfos info = new ContactInfos();
+                String emailAddress = emails.getString(emails.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Email.DATA));
+                String emailType = emails.getString(emails.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Email.TYPE));
+                info.setEmailOrNumber(1);
+                info.setNumber(emailAddress);
+                info.setType(emailType);
+                contactInfos.add(info);
+            }
+            emails.close();
+            contact.setContactInfos(contactInfos);
+            contacts.add(contact);
+        }
+        for(int i=0;i<contacts.size();i++){
+            if(contacts.get(i).getContactInfos().size()==0){
+                contacts.remove(i);
+                i--;
+            }
+        }
+        cursor.close();
+        return contacts;
+    }
+
+    private List<Record> GetRecordFromLocal() {
+        List<Record> records = new ArrayList<Record>();
+        ContentResolver contentResolver = this.getContentResolver();
+        //获取权限
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.READ_CALL_LOG,
+                        Manifest.permission.WRITE_CONTACTS,
+                        Manifest.permission.WRITE_CALL_LOG,
+                        Manifest.permission.READ_CALL_LOG,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.CALL_PHONE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                }, 1012);
+            }
+        }
+
+        Cursor recor = contentResolver.query(CallLog.Calls.CONTENT_URI,
+                null,
+                null,
+                null, null);
+        while (recor.moveToNext()) {
+            Record info = new Record();
+            int type = recor.getInt(recor.getColumnIndex(CallLog.Calls.TYPE));
+            String number = recor.getString(recor.getColumnIndex(
+                    CallLog.Calls.NUMBER));
+            String duration = recor.getString(recor.getColumnIndex(
+                    CallLog.Calls.DURATION));
+            String date = recor.getString(recor.getColumnIndex(
+                    CallLog.Calls.DATE));
+            info.setNumber(number);
+            info.setDuration(duration);
+            info.setDate(date);
+            info.setType(type + "");
+            records.add(info);
+        }
+        for(int i=0;i<records.size();i++){
+            if(records.get(i).getNumber().isEmpty()){
+                records.remove(i);
+                i--;
+            }
+        }
+        recor.close();
+        return records;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
